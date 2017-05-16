54f23141e470c2497f9d3f5dad8974a3cf4414c1
Tamas K. Papp <tkpapp@gmail.com>
2017-05-16 11:53:02 +0200

    Added basic MCMCDraws definitions.

--- src/MCMCSamples.jl
+++ src/MCMCSamples.jl
@@ -1,5 +1,37 @@
 module MCMCSamples
 
-# package code goes here
+using ArgCheck
 
-end # module
+export
+    MCMCDraws
+
+######################################################################
+# collections of draws
+######################################################################
+
+const MCMCDrawsVector = AbstractVector
+
+const MCMCDrawsDict = Dict{Symbol, MCMCDrawsVector}
+
+struct MCMCDraws
+    dict::MCMCDrawsDict
+    function MCMCDraws(dict)
+        @argcheck !isempty(dict) "Need at least one vector of draws."
+        N = length(first(values(dict)))
+        @argcheck all(length(x) == N for x in values(dict)) "Inconsistent vector length."
+        new(dict)
+    end
+end
+
+MCMCDraws(pairs::Pair{Symbol, <:AbstractArray}...) = MCMCDraws(MCMCDrawsDict(pairs...))
+
+Base.length(draws::MCMCDraws) = length(first(values(draws.dict)))
+
+Base.keys(draws::MCMCDraws) = sort!(collect(keys(draws.dict)))
+
+Base.getindex(draws::MCMCDraws, var::Symbol) = draws.dict[var]
+
+function is_same_vars(draws1::MCMCDraws, draws_rest::MCMCDraws...)
+    vn1 = varnames(draws1)
+    all(varnames(draws) == v1 for draws in draws_rest)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
 using MCMCSamples
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "mcmcdraws" begin
+    va = 1:10
+    vb = randn(length(va))
+    draws = MCMCDraws(:b => vb, :a => va)
+    @test keys(draws) == [:a, :b] # order important
+    @test draws[:a] == va
+    @test draws[:b] == vb
+    @test length(draws) == length(va)
+    @test MCMCDraws(:a => va, :b => vb).dict == draws.dict
+    @test_throws ArgumentError MCMCDraws()
+    @test_throws ArgumentError MCMCDraws(:a => va, :b => vb, :c => 1:8)
+end
